fc8e9c7389a75ce14a5a5c5993c0ce46f395c9e6
Noesax <marko.rique@unmsm.edu.pe>
2023-09-14 15:12:28 -0500

    PRIMER BACK

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "signup"
+})
+
+
+app.post('/registro', (req, res)=>{
+    const sql = "INSERT INTO login('name', 'email', 'password') VALUES (?)";
+    const values = [
+        req.body.name,
+        req.body.email,
+        req.body.password
+    ]
+    db.query(sql, [values], (err, data)=>{
+        if(err){
+            return res.json("Error");
+        }
+        return res.json(data);
+    })
+})
+
+app.listen(8081, ()=>{
+    console.log("listening");
+})
