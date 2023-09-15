0bd83aca7e17838df60fec31fec0fcf5132538a2
Noesax <marko.rique@unmsm.edu.pe>
2023-09-15 11:18:45 -0500

    Me confundí, esto debería estar en mi rama xd

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,36 @@
-const express = require("express");
-const mysql = require('mysql');
+const { Client } = require('pg');
+const express = require('express');
 const cors = require('cors');
 
 const app = express();
+
 app.use(cors());
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "signup"
-})
+const connectionData = {
+    user: 'postgres',
+    host: 'localhost',
+    database: 'telecomunicaciones',
+    password: 'Jjba222716',
+    port: 5432,
+}
 
+const client = new Client(connectionData)
 
-app.post('/registro', (req, res)=>{
-    const sql = "INSERT INTO login('name', 'email', 'password') VALUES (?)";
-    const values = [
-        req.body.name,
-        req.body.email,
-        req.body.password
-    ]
-    db.query(sql, [values], (err, data)=>{
-        if(err){
-            return res.json("Error");
-        }
-        return res.json(data);
+client.connect();
+
+app.get('/clientes', (req, res) => {
+    client.query('SELECT * FROM cliente')
+    .then(response => {
+        console.log(response.rows);
+        client.end();
+        res.json(response.rows);
     })
-})
+    .catch(err => {
+        client.error(err);
+        res.status(500),json({ error: 'Error en la consulta a la base de datos'});
+    });
+});
 
 app.listen(8081, ()=>{
-    console.log("listening");
+    console.log("listening en el puerto 8081");
 })
