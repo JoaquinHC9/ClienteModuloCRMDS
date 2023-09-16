b25a83ef4f549e02a18ec99ec74fdc52d6b80c68
Noesax <marko.rique@unmsm.edu.pe>
2023-09-15 21:25:23 -0500

    MODULO 1 FUNCIONAL

--- backend/server.js
+++ backend/server.js
@@ -18,17 +18,33 @@ const client = new Client(connectionData)
 
 client.connect();
 
-app.get('/clientes', (req, res) => {
+app.get('/registerAPI', (req, res) => {
     client.query('SELECT * FROM cliente')
     .then(response => {
         console.log(response.rows);
-        client.end();
         res.json(response.rows);
     })
     .catch(err => {
         client.error(err);
         res.status(500),json({ error: 'Error en la consulta a la base de datos'});
     });
+});
+
+app.use(express.json());
+
+app.post('/registerAPI', (req, res)=>{
+    const {dni, nombre, apellido, fechaNacimiento, distrito, departamento, fechaAfiliacion, email} = req.body;
+    const query = 'INSERT INTO cliente (dni, nombre, apellido, fechanac, distrito, departamento, fechaafili, correo) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)';
+    const values = [dni, nombre, apellido, fechaNacimiento, distrito, departamento, fechaAfiliacion, email];
+
+    client.query(query, values)
+        .then(() => {
+            res.status(201).json({ message: 'Registro exitoso '});
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({error: 'Error en el registro'});
+        })
 });
 
 app.listen(8081, ()=>{
