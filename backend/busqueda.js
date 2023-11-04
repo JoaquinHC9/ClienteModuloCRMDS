7cc8dbd9376d961d5b8cfbee61a9d94deb36747f
Noesax <marko.rique@unmsm.edu.pe>
2023-11-03 22:29:13 -0500

    Modificando

--- backend/busqueda.js
+++ backend/busqueda.js
@@ -3,80 +3,91 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-
 app.use(cors());
 
 const connectionData = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'telecomunicaciones',
-    password: 'Jjba222716',
-    port: 5432,
+  user: 'postgres',
+  host: 'localhost',
+  database: 'telecomunicaciones',
+  password: 'Jjba222716',
+  port: 5432,
+};
+
+class DatabaseProxy {
+  constructor(connectionData) {
+    this.client = new Client(connectionData);
+    this.client.connect();
+  }
+
+  query(sql, values) {
+    return this.client.query(sql, values);
+  }
 }
-const client = new Client(connectionData)
 
-client.connect();
+const dbProxy = new DatabaseProxy(connectionData);
+
 app.use(express.json());
-//busqueda clientes
+
 app.get('/buscarPorDNI/:dni', (req, res) => {
-    const dniABuscar = req.params.dni;
-    const query = 'SELECT * FROM cliente WHERE dni = $1';
+  const dniABuscar = req.params.dni;
+  const query = 'SELECT * FROM cliente WHERE dni = $1';
 
-    client.query(query, [dniABuscar])
-        .then(response => {
-            if (response.rows.length === 0) {
-                console.log('No se encontraron resultados para el DNI: ' + dniABuscar);
-            } else {
-                console.log('Resultados de la búsqueda por DNI ' + dniABuscar + ':');
-                console.log(response.rows);
-            }
-            res.json(response.rows);
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({ error: 'Error en la consulta a la base de datos' });
-        });
-});
-app.get('/buscarPorNombre/:nombre', (req, res) => {
-    const nombreABuscar = req.params.nombre;
-    const query = 'SELECT * FROM cliente WHERE nombre = $1';
-
-    client.query(query, [nombreABuscar])
-        .then(response => {
-            if (response.rows.length === 0) {
-                console.log('No se encontraron resultados para el nombre: ' + nombreABuscar);
-            } else {
-                console.log('Resultados de la búsqueda por nombre ' + nombreABuscar + ':');
-                console.log(response.rows);
-            }
-            res.json(response.rows);
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({ error: 'Error en la consulta a la base de datos' });
-        });
-});
-//busqueda datos del cliente detalle
-app.get('/buscarClienteDetalladoPorDNI/:dni', (req, res) => {
-    const dniABuscar = req.params.dni;
-    const query = 'SELECT * FROM clienteDetallado WHERE dni = $1';
-
-    client.query(query, [dniABuscar])
+  dbProxy.query(query, [dniABuscar])
     .then(response => {
-        if (response.rows.length === 0) {
-        console.log('No se encontraron resultados en clienteDetallado para el DNI: ' + dniABuscar);
-        } else {
-        console.log('Resultados de la búsqueda en clienteDetallado por DNI ' + dniABuscar + ':');
+      if (response.rows.length === 0) {
+        console.log('No se encontraron resultados para el DNI: ' + dniABuscar);
+      } else {
+        console.log('Resultados de la búsqueda por DNI ' + dniABuscar + ':');
         console.log(response.rows);
-        }
-        res.json(response.rows);
+      }
+      res.json(response.rows);
     })
     .catch(err => {
-        console.error(err);
-        res.status(500).json({ error: 'Error en la consulta a la base de datos en clienteDetallado' });
+      console.error(err);
+      res.status(500).json({ error: 'Error en la consulta a la base de datos' });
+    });
+});
+
+app.get('/buscarPorNombre/:nombre', (req, res) => {
+  const nombreABuscar = req.params.nombre;
+  const query = 'SELECT * FROM cliente WHERE nombre = $1';
+
+  dbProxy.query(query, [nombreABuscar])
+    .then(response => {
+      if (response.rows.length === 0) {
+        console.log('No se encontraron resultados para el nombre: ' + nombreABuscar);
+      } else {
+        console.log('Resultados de la búsqueda por nombre ' + nombreABuscar + ':');
+        console.log(response.rows);
+      }
+      res.json(response.rows);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Error en la consulta a la base de datos' });
+    });
+});
+
+app.get('/buscarClienteDetalladoPorDNI/:dni', (req, res) => {
+  const dniABuscar = req.params.dni;
+  const query = 'SELECT * FROM clienteDetallado WHERE dni = $1';
+
+  dbProxy.query(query, [dniABuscar])
+    .then(response => {
+      if (response.rows.length === 0) {
+        console.log('No se encontraron resultados en clienteDetallado para el DNI: ' + dniABuscar);
+      } else {
+        console.log('Resultados de la búsqueda en clienteDetallado por DNI ' + dniABuscar + ':');
+        console.log(response.rows);
+      }
+      res.json(response.rows);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Error en la consulta a la base de datos en clienteDetallado' });
     });
 });
 
 app.listen(8081, () => {
-    console.log("listening en el puerto 8081");
-})
+  console.log("listening en el puerto 8081");
+});
--- backend/modificarCliente.js
+++ backend/modificarCliente.js
@@ -3,50 +3,65 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-
 app.use(cors());
 
 const connectionData = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'telecomunicaciones',
-    password: 'Jjba222716',
-    port: 5432,
+  user: 'postgres',
+  host: 'localhost',
+  database: 'telecomunicaciones',
+  password: 'Jjba222716',
+  port: 5432,
+};
+
+class DatabaseProxy {
+  constructor(connectionData) {
+    this.client = new Client(connectionData);
+    this.client.connect();
+  }
+
+  query(sql, values) {
+    return this.client.query(sql, values);
+  }
+
+  update(sql, values) {
+    return this.client.query(sql, values);
+  }
 }
-const client = new Client(connectionData)
 
-client.connect();
+const dbProxy = new DatabaseProxy(connectionData);
+
 app.use(express.json());
-//modificar perfil datos del cliente detalle, falta modificar datos del cliente
+
 app.put('/api/clientesDetallados/:dni', (req, res) => {
-    const dniActualizar = req.params.dni;
-    const { codigoPostal, trabajo, hobie, estadoCivil, numHijos, contacExterno } = req.body;
+  const dniActualizar = req.params.dni;
+  const { codigoPostal, trabajo, hobie, estadoCivil, numHijos, contacExterno } = req.body;
 
-    // Verifica si el cliente detallado existe antes de actualizar
-    const queryExistencia = 'SELECT * FROM clienteDetallado WHERE dni = $1';
-    client.query(queryExistencia, [dniActualizar])
-        .then(response => {
-            if (response.rows.length === 0) {
-                res.status(404).json({ error: 'Cliente detallado no encontrado' });
-            } else {
-                const query = 'UPDATE clienteDetallado SET codigoPostal = $1, trabajo = $2, hobie = $3, estadoCivil = $4, numHijos = $5, contacExterno = $6 WHERE dni = $7';
-                const values = [codigoPostal, trabajo, hobie, estadoCivil, numHijos, contacExterno, dniActualizar];
+  // Verifica si el cliente detallado existe antes de actualizar
+  const queryExistencia = 'SELECT * FROM clienteDetallado WHERE dni = $1';
+  dbProxy.query(queryExistencia, [dniActualizar])
+    .then(response => {
+      if (response.rows.length === 0) {
+        res.status(404).json({ error: 'Cliente detallado no encontrado' });
+      } else {
+        const query = 'UPDATE clienteDetallado SET codigoPostal = $1, trabajo = $2, hobie = $3, estadoCivil = $4, numHijos = $5, contacExterno = $6 WHERE dni = $7';
+        const values = [codigoPostal, trabajo, hobie, estadoCivil, numHijos, contacExterno, dniActualizar];
 
-                client.query(query, values)
-                    .then(() => {
-                        res.status(200).json({ message: 'Actualización exitosa' });
-                    })
-                    .catch(err => {
-                        console.error(err);
-                        res.status(500).json({ error: 'Error en la actualización' });
-                    });
-            }
-        })
-        .catch(err => {
+        dbProxy.update(query, values)
+          .then(() => {
+            res.status(200).json({ message: 'Actualización exitosa' });
+          })
+          .catch(err => {
             console.error(err);
-            res.status(500).json({ error: 'Error en la consulta a la base de datos' });
-        });
+            res.status(500).json({ error: 'Error en la actualización' });
+          });
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Error en la consulta a la base de datos' });
+    });
 });
+
 app.listen(8081, () => {
-    console.log("listening en el puerto 8081");
-})
+  console.log("listening en el puerto 8081");
+});
