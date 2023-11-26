eb47dd962d159ca485f5663c6232dfbdae51bd7d
Joaquin Hidalgo <joaquin.hidalgo@unmsm.edu.pe>
2023-11-26 09:38:39 -0500

    req marketing

--- frontend/src/pages/Registro.js
+++ frontend/src/pages/Registro.js
@@ -115,7 +115,17 @@ export default function Registro() {
               <label htmlFor='dni'>
                 <strong>DNI</strong>
               </label>
-              <input type='text' placeholder='Digite DNI' onChange={onChange} required value={clienteData.dni} name='dni' className='form-control rounded-0' style={{ width: '300px' }}></input>
+              <input
+                type='text'
+                placeholder='Digite DNI'
+                onChange={onChange}
+                maxLength={8} // Limita la longitud a 8 caracteres
+                required
+                value={clienteData.dni}
+                name='dni'
+                className='form-control rounded-0'
+                style={{ width: '300px' }}
+              />
             </div>
           </div>
           <div className='mb-2 d-flex justify-content-between'>
