14ebe9bed24cb32895d545fd1e193bf9c2e2350d
Joaquin Hidalgo <joaquin.hidalgo@unmsm.edu.pe>
2023-11-26 10:44:06 -0500

    leiminado lineas (pertenecen a ventas)

--- backend/routes/lineaRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const lineaController = require('../controllers/lineaController');
-
-router.get('/buscarLineasPorDNI/:dni', lineaController.buscarLineasPorDNI);
-router.get('/obtenerDetallesDeLinea/:numTelefono', lineaController.obtenerDetallesDeLinea);
-router.get('/dniPorNumeroTelefono/:numTelefono', lineaController.dniPorNumeroTelefono);
-
-module.exports = router;
