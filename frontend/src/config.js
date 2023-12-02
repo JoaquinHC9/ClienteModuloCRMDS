2551a2f61e3024add841988d2b6673d416efb50b
Joaquin Hidalgo <joaquin.hidalgo@unmsm.edu.pe>
2023-12-01 22:39:11 -0500

    xd

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:8081'
-//export const API_URL = 'https://clientemodulocrm.onrender.com'
+//export const API_URL = 'http://localhost:8081'
+export const API_URL = 'https://clientemodulocrm.onrender.com'
 export const VENTAS_URL = 'https://modulo-ventas.onrender.com'
